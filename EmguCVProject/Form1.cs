bddca368603741e82597a640c3edf04cf02d40b1
AndreiDaraban <andrei.daraban70@yahoo.com>
2022-05-15 12:39:52 +0300

    First app
    -added a button
    -added a picturebox
    -added a click event
    -wrote some code to open an image

--- EmguCVProject/Form1.cs
+++ EmguCVProject/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,17 @@ namespace EmguCVProject
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
+                pictureBox1.Image = My_Image.ToBitmap();
+            }
+
         }
     }
 }
