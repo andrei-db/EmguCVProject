234bc8059134962a78e431a9643c512b79101d92
AndreiDaraban <andrei.daraban70@yahoo.com>
2022-05-15 12:54:06 +0300

    Convert to grayscale
    -converted an image to a gray image

--- EmguCVProject/Form1.Designer.cs
+++ EmguCVProject/Form1.Designer.cs
@@ -29,48 +29,75 @@ namespace EmguCVProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.openImageBtn = new System.Windows.Forms.Button();
+            this.imageArea = new System.Windows.Forms.PictureBox();
+            this.grayImageBtn = new System.Windows.Forms.Button();
+            this.grayImageArea = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageArea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grayImageArea)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // openImageBtn
             // 
-            this.button1.Location = new System.Drawing.Point(219, 486);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.openImageBtn.Location = new System.Drawing.Point(192, 471);
+            this.openImageBtn.Name = "openImageBtn";
+            this.openImageBtn.Size = new System.Drawing.Size(106, 23);
+            this.openImageBtn.TabIndex = 0;
+            this.openImageBtn.Text = "Choose an image";
+            this.openImageBtn.UseVisualStyleBackColor = true;
+            this.openImageBtn.Click += new System.EventHandler(this.openImageBtn_Click);
             // 
-            // pictureBox1
+            // imageArea
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(74, 158);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(353, 291);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.imageArea.Location = new System.Drawing.Point(74, 158);
+            this.imageArea.Name = "imageArea";
+            this.imageArea.Size = new System.Drawing.Size(353, 291);
+            this.imageArea.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageArea.TabIndex = 1;
+            this.imageArea.TabStop = false;
+            // 
+            // grayImageBtn
+            // 
+            this.grayImageBtn.Location = new System.Drawing.Point(687, 471);
+            this.grayImageBtn.Name = "grayImageBtn";
+            this.grayImageBtn.Size = new System.Drawing.Size(75, 23);
+            this.grayImageBtn.TabIndex = 2;
+            this.grayImageBtn.Text = "Make it gray";
+            this.grayImageBtn.UseVisualStyleBackColor = true;
+            this.grayImageBtn.Click += new System.EventHandler(this.grayImageBtn_Click);
+            // 
+            // grayImageArea
+            // 
+            this.grayImageArea.Location = new System.Drawing.Point(541, 158);
+            this.grayImageArea.Name = "grayImageArea";
+            this.grayImageArea.Size = new System.Drawing.Size(320, 291);
+            this.grayImageArea.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.grayImageArea.TabIndex = 3;
+            this.grayImageArea.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(919, 651);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grayImageArea);
+            this.Controls.Add(this.grayImageBtn);
+            this.Controls.Add(this.imageArea);
+            this.Controls.Add(this.openImageBtn);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageArea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grayImageArea)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openImageBtn;
+        private System.Windows.Forms.PictureBox imageArea;
+        private System.Windows.Forms.Button grayImageBtn;
+        private System.Windows.Forms.PictureBox grayImageArea;
     }
 }
 
--- EmguCVProject/Form1.cs
+++ EmguCVProject/Form1.cs
@@ -18,15 +18,23 @@ namespace EmguCVProject
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        Image<Bgr, Byte> My_Image;
+        private void openImageBtn_Click(object sender, EventArgs e)
         {
             OpenFileDialog Openfile = new OpenFileDialog();
             if (Openfile.ShowDialog() == DialogResult.OK)
             {
-                Image<Bgr, Byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
-                pictureBox1.Image = My_Image.ToBitmap();
+                My_Image = new Image<Bgr, byte>(Openfile.FileName);
+                imageArea.Image = My_Image.ToBitmap();
             }
+
+        }
+
+        private void grayImageBtn_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> gray_image = My_Image.Convert<Gray, byte>();
+            grayImageArea.Image = gray_image.AsBitmap();
+            gray_image[0, 0] = new Gray(200);
 
         }
     }
